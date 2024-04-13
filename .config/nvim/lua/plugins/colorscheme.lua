c0a1ac868112fc401ae6e8cb3974b2be612822d7
Valerius Mattfeld <mail@valerius.me>
2024-04-13 23:15:12 +0200

    change default colorscheme

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,14 @@
+return {
+  { "fxn/vim-monochrome", name = "monochrome" },
+  { "bluz71/vim-moonfly-colors", name = "moonfly" },
+  { "sainnhe/sonokai", name = "sonokai" },
+  { "olimorris/onedarkpro.nvim", name = "onedarkpro" },
+  { "projekt0n/github-nvim-theme", name = "github" },
+  { "rebelot/kanagawa.nvim", name = "kanagawa" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
