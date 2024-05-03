edd291e7b1288d69c250e150ee360022f2b9b484
Valerius Mattfeld <mail@valerius.me>
2024-05-03 21:33:06 +0200

    add biome_init function

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -11,47 +11,46 @@ return {
             client.server_capabilities.documentFormattingProvider = false
           end,
         },
-        biome = {},
-        -- other language servers
       },
     },
   },
   {
     "nvimtools/none-ls.nvim",
-    opts = function(_, opts)
-      local nls = require("null-ls").builtins
-      opts.sources = vim.list_extend(opts.sources or {}, {
-        nls.formatting.biome,
-
-        -- or if you like to live dangerously like me:
-        nls.formatting.biome.with({
-          args = {
-            "check",
-            "--apply-unsafe",
-            "--formatter-enabled=true",
-            "--organize-imports-enabled=true",
-            "--skip-errors",
-            "$FILENAME",
-          },
-        }),
-      })
-    end,
+    opts = {},
+    --   function(_, opts)
+    --   local nls = require("null-ls").builtins
+    --   opts.sources = vim.list_extend(opts.sources or {}, {
+    --     nls.formatting.biome,
+    --
+    --     -- or if you like to live dangerously like me:
+    --     nls.formatting.biome.with({
+    --       args = {
+    --         "check",
+    --         "--apply-unsafe",
+    --         "--formatter-enabled=true",
+    --         "--organize-imports-enabled=true",
+    --         "--skip-errors",
+    --         "$FILENAME",
+    --       },
+    --     }),
+    --   })
+    -- end,
   },
   {
     "stevearc/conform.nvim",
     optional = true,
     opts = {
       formatters_by_ft = {
-        ["javascript"] = { "biome" },
-        ["javascriptreact"] = { "biome" },
-        ["typescript"] = { "biome" },
-        ["typescriptreact"] = { "biome" },
+        ["javascript"] = { "prettier" },
+        ["javascriptreact"] = { "prettier" },
+        ["typescript"] = { "prettier" },
+        ["typescriptreact"] = { "prettier" },
         ["vue"] = { "prettier" },
         ["css"] = { "prettier" },
         ["scss"] = { "prettier" },
         ["less"] = { "prettier" },
         ["html"] = { "prettier" },
-        ["json"] = { "biome" },
+        ["json"] = { "prettier" },
         ["jsonc"] = { "prettier" },
         ["yaml"] = { "prettier" },
         ["markdown"] = { "prettier" },
--- .functions.zsh
+++ .functions.zsh
@@ -1,3 +1,64 @@
+#!/bin/zsh
+
+function biome_init() {
+  # This script installs biome in the current repository and migrates exisitng prettier/eslint configs
+  echo "Initializing biome for repository $(pwd)"
+  if [[ ! -d node_modules ]]; then
+    echo "node_modules not present. please install them and then run the command again"
+    return 1
+  # Figure out, which package manager is used
+  PACKAGE_MANAGER=""
+
+  elif [[ -f package-lock.json ]]; then
+    echo "Found NPM. Running"
+    PACKAGE_MANAGER=`which npm`
+    if [[ ! -f "$PACKAGE_MANAGER" ]]; then
+      echo "Cannot find $PACKAGE_MANAGER"
+      return 1
+    fi
+    npm install --save-dev --save-exact @biomejs/biome
+    npx biome init
+    npx biome migrate eslint --write
+    npx biome migrate prettier --write
+  elif [[ -f bun.lockb ]]; then
+    echo "Found Bun. Running..."
+    PACKAGE_MANAGER=`which bun`
+    if [[ ! -f "$PACKAGE_MANAGER" ]]; then
+      echo "Cannot find $PACKAGE_MANAGER"
+      return 1
+    fi
+    bun add --dev --exact @biomejs/biome
+    bunx biome init
+    bunx biome migrate eslint --write
+    bunx biome migrate prettier --write
+  elif [[ -f pnpm-lock.json ]]; then
+    echo "Found PNPM. Running..."
+    PACKAGE_MANAGER=`which pnpm`
+    if [[ ! -f "$PACKAGE_MANAGER" ]]; then
+      echo "Cannot find $PACKAGE_MANAGER"
+      return 1
+    fi
+    pnpm add --save-dev --save-exact @biomejs/biome
+    pnpm biome init
+    pnpm biome migrate eslint --write
+    pnpm biome migrate prettier --write
+  elif [[ -f yarn.lock ]]; then
+    echo "Found Yarn. Running..."
+    PACKAGE_MANAGER=`which yarn`
+    if [[ ! -f "$PACKAGE_MANAGER" ]]; then
+      echo "Cannot find $PACKAGE_MANAGER"
+      return 1
+    fi
+    yarn add --dev --exact @biomejs/biome
+    yarn biome init
+    yarn biome migrate eslint --write
+    yarn biome migrate prettier --write
+  else
+    echo "Package Manager unknown. Exiting..."
+    return 1
+  fi
+  return 0
+}
 
 function setup_venv() {
     if [[ ! -d .venv ]]; then 
