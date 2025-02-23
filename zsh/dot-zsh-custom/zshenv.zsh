b6397034474e0d9d437b39a07a537c7a8da24ca4
Valerius Mattfeld <mail@valerius.me>
2025-02-23 16:40:01 +0100

    updat env

--- zsh/dot-zsh-custom/zshenv.zsh
+++ zsh/dot-zsh-custom/zshenv.zsh
@@ -1,3 +1,6 @@
+# PATH 
+export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
+
 # Cargo
 . "$HOME/.cargo/env"
 
@@ -5,3 +8,11 @@
 export ANTHROPIC_API_KEY=$(pass show api/ANTHROPIC_API_KEY 2>/dev/null)
 export DEEPSEEK_API_KEY=$(pass show api/DEEPSEEK_API_KEY 2>/dev/null)
 
+# Etc
+export LANG=en_US.UTF-8
+
+if [[ -n $SSH_CONNECTION ]]; then
+  export EDITOR='vim'
+else
+  export EDITOR='nvim'
+fi
